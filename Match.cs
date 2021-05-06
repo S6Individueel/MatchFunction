377b7f9b61ee4590d2f674e8653767198acd28b4
I384733 <I384733@fhict.nl>
2021-05-06 19:12:43 +0200

    Added notification system/calls to update every user on the current amount of users, as well as their names, with a random entry message from the server. Updating goes from client --> server --> host --> server --> all clients where the host keeps track of all users since the creation of the room. SignalR serverless doesn't support user count so it's being tracked client side which is okay because only the numbers and names are being tracked client side. The group with users in it is still tracked serverlessly. (server side)

--- Match.cs
+++ Match.cs
@@ -11,13 +11,16 @@ using System.Net.Http;
 using MatchFunction.Model;
 using Microsoft.Azure.WebJobs.Extensions.SignalRService;
 using System.Security.Claims;
+using System.Collections.Generic;
+
 namespace MatchFunction
 {
     public class Match 
     {
 
         private static HttpClient httpClient = new HttpClient();
-
+        private static Random rnd = new Random();
+        private static readonly string[] sentences = new string[] { "momentarily", "bloodthirstily", "unnecessarily", "trustworthily", "involuntarily", "secondarily", "mandatorily", "temporarily", "arbitrarily", "voluntarily", "ordinarily", "stealthily", "unsanitarily", "worthily", "unworthily", "sanitarily", "squekily", "hungrily", "cheekily" };
         [FunctionName("Negotiate")]
         public static SignalRConnectionInfo Run(
         [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "negotiate")] HttpRequest req,
@@ -32,8 +35,21 @@ namespace MatchFunction
         [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "{group}/add/{userId}")] HttpRequest req,
         string group,
         string userId,
-        [SignalR(HubName = "chat")] IAsyncCollector<SignalRGroupAction> signalRGroupActions)
+        [SignalR(HubName = "chat")] IAsyncCollector<SignalRGroupAction> signalRGroupActions/*,
+        [SignalR(HubName = "chat")] IAsyncCollector<SignalRMessage> signalRMessages*/)
         {
+/*            ChatMessage chatMessage = new ChatMessage();
+            chatMessage.Content = "has joined the room " + sentences[rnd.Next(sentences.Length)];
+            chatMessage.UserName = userId;
+
+            signalRMessages.AddAsync(
+                new SignalRMessage
+                {
+                    GroupName = group,
+                    Target = "incomingUser",
+                    Arguments = new[] { chatMessage }
+                });*/
+
             return signalRGroupActions.AddAsync(
                 new SignalRGroupAction
                 {
@@ -57,7 +73,7 @@ namespace MatchFunction
                     UserId = userId,
                     GroupName = groupCode,
                     Action = GroupAction.Add
-                });
+                }); 
 
             return signalRMessages.AddAsync( //Return the message with the code and user in order for quick initialization client side, also reduces roundtrip by 1
             new SignalRMessage
@@ -102,6 +118,45 @@ namespace MatchFunction
                     GroupName = group,
                     Target = group,
                     Arguments = new[] { chatMessage }
+                });
+        }
+
+        [FunctionName("SendUpdateToGroup")]
+        public static Task SendUpdateToGroup(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "{group}/updategroup")] object message, //contains chatmessage with latest user, userslist in room and the funny
+        string group,
+        [SignalR(HubName = "chat")] IAsyncCollector<SignalRMessage> signalRMessages)
+        {
+            ChatMessage updateMessage = JsonConvert.DeserializeObject<ChatMessage>(message.ToString());
+
+            return signalRMessages.AddAsync(
+                new SignalRMessage
+                {
+                    GroupName = group,
+                    Target = "incomingUser",
+                    Arguments = new[] { updateMessage }
+                });
+        }
+
+        [FunctionName("SendUpdateToHost")]
+        public static Task SendUpdateToHost(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "{group}/updatehost")] object message, //contains username to update
+        string group,
+        [SignalR(HubName = "chat")] IAsyncCollector<SignalRMessage> signalRMessages)
+        {
+
+            ChatMessage updateMessage = new ChatMessage
+            {
+                UserName = message.ToString(),
+                Content = "has joined the room " + sentences[rnd.Next(sentences.Length)]
+            };
+
+            return signalRMessages.AddAsync(
+                new SignalRMessage
+                {
+                    GroupName = group,
+                    Target = "incomingUserUpdate",
+                    Arguments = new[] { updateMessage }
                 });
         }
 
--- Model/ChatMessage.cs
+++ Model/ChatMessage.cs
@@ -6,7 +6,18 @@ namespace MatchFunction.Model
 {
     public class ChatMessage
     {
+        public ChatMessage(string _userName, string _content)
+        {
+            UserName = _userName;
+            Content = _content;
+        }
+        public ChatMessage()
+        {
+
+        }
+
         public string UserName { get; set; }
         public string Content { get; set; }
+        public List<string> UsersList { get; set; }
     }
 }
